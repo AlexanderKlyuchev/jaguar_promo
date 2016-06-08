a737f8a440ac095739bdf1ad3b8f4cd0d74a653a
Alexander Klyuchev <aklyuchevsq@gmail.com>
2016-06-08 17:03:03 +0300

    Initial Commit

---
+++ JaguarPromo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JaguarPromo
+//
+//  Created by Александр Ключев on 6/8/16.
+//  Copyright © 2016 Александр Ключев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
