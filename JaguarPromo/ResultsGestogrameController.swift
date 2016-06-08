15faabfeebed2f089b09ec4262bb0ce15de83c1a
Alexander Klyuchev <aklyuchevsq@gmail.com>
2016-06-08 18:33:52 +0300

    project init

---
+++ JaguarPromo/ResultsGestogrameController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsGestogrameController.swift
+//  JaguarPromo
+//
+//  Created by Александр Ключев on 6/8/16.
+//  Copyright © 2016 Александр Ключев. All rights reserved.
+//
+
+import UIKit
+
+class ResultsGestogrameController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- JaguarPromo/ViewController.swift
+++ JaguarPromo/ViewController.swift
@@ -6,13 +6,45 @@
 //  Copyright © 2016 Александр Ключев. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Load the video from the app bundle.
+        let videoURL: NSURL = NSBundle.mainBundle().URLForResource("mov_start_01", withExtension: "mp4")!
+        
+        player = AVPlayer(URL: videoURL)
+        player?.actionAtItemEnd = .None
+        player?.muted = true
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.zPosition = -1
+        
+        playerLayer.frame = view.frame
+        
+        view.layer.addSublayer(playerLayer)
+        
+        player?.play()
+        
+        //loop video
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(ViewController.loopVideo),
+                                                         name: AVPlayerItemDidPlayToEndTimeNotification,
+                                                         object: nil)
+        
+        
+    }
+    
+    func loopVideo() {
+        player?.seekToTime(kCMTimeZero)
+        player?.play()
     }
 
     override func didReceiveMemoryWarning() {
